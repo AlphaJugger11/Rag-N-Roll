8a4540a604ffc4521aaabe844c504d07ee27b50d
Uriba Parveen Khan <111582983+uribakhan1212@users.noreply.github.com>
2025-01-12 01:23:25 +0500

    Updated

--- app.py
+++ app.py
@@ -21,18 +21,88 @@ st.title("Interactive Chatbot")
 # Initialize session state for chat messages
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    
+# *****************************Adding functions*********************************
+num_chunks = 3
+def create_prompt (myquestion, rag):
+    if rag == 1:    
+
+        cmd = """
+         with results as
+         (SELECT RELATIVE_PATH,
+           VECTOR_COSINE_SIMILARITY(docs_chunks_table.chunk_vec,
+                    SNOWFLAKE.CORTEX.EMBED_TEXT_768('snowflake-arctic-embed-m', ?)) as similarity,
+           chunk
+         from docs_chunks_table
+         order by similarity desc
+         limit ?)
+         select chunk, relative_path from results 
+         """
+    
+        df_context = session.sql(cmd, params=[myquestion, num_chunks]).to_pandas()      
+        
+        context_lenght = len(df_context) -1
+
+        prompt_context = ""
+        for i in range (0, context_lenght):
+            prompt_context += df_context._get_value(i, 'CHUNK')
+
+        prompt_context = prompt_context.replace("'", "")
+        relative_path =  df_context._get_value(0,'RELATIVE_PATH')
+    
+        prompt = f"""
+          'You are an expert legal assistance extracting information from context provided. 
+           Answer the question based on the context. Be concise and do not hallucinate. 
+           If you don´t have the information just say so.
+          Context: {prompt_context}
+          Question:  
+           {myquestion} 
+           Answer: '
+           """
+        cmd2 = f"select GET_PRESIGNED_URL(@docs, '{relative_path}', 360) as URL_LINK from directory(@docs)"
+        df_url_link = session.sql(cmd2).to_pandas()
+        url_link = df_url_link._get_value(0,'URL_LINK')
+
+    else:
+        prompt = f"""
+         'Question:  
+           {myquestion} 
+           Answer: '
+           """
+        url_link = "None"
+        relative_path = "None"
+      
+    return prompt, url_link, relative_path
+
+def complete(myquestion, model_name, rag = 1):
+    prompt, url_link, relative_path =create_prompt (myquestion, rag)
+    cmd = f"""
+             select SNOWFLAKE.CORTEX.COMPLETE(?,?) as response
+           """   
+    df_response = session.sql(cmd, params=['mistral-large', prompt]).collect()
+    return df_response, url_link, relative_path
+
+def get_response (question, rag=0):
+    model = 'mistral-large'
+    response, url_link, relative_path = complete(question, model, rag)
+    res_text = response[0].RESPONSE
+    st.markdown(res_text)
+    if rag == 1:
+        display_url = f"Link to [{relative_path}]({url_link}) that may be useful"
+        st.markdown(display_url)
+    return res_text
 
 # Function to generate a response using the Hugging Face model
-def get_response(messages):
-    """
-    Generate a response using the Hugging Face Mistral model.
-    """
-    completion = client.chat.completions.create(
-        model="mistralai/Mistral-Nemo-Instruct-2407",
-        messages=messages,
-        max_tokens=4096
-    )
-    return completion["choices"][0]["message"]["content"]
+# def get_response(messages):
+#     """
+#     Generate a response using the Hugging Face Mistral model.
+#     """
+#     completion = client.chat.completions.create(
+#         model="mistralai/Mistral-Nemo-Instruct-2407",
+#         messages=messages,
+#         max_tokens=4096
+#     )
+#     return completion["choices"][0]["message"]["content"]
 
 # Display previous chat messages
 for message in st.session_state.messages:
